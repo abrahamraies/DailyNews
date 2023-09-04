846394a0ae0232c020adab2b06a6992c2b05a813
abrahamraies <abrahamraies@gmail.com>
2023-09-03 21:04:43 -0300

    Modify the url of api

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -10,7 +10,7 @@ export class NewsService {
   constructor(private http: HttpClient) { }
 
   getNews(params: any): Observable<any> {
-    const url = 'https://newsapi.org/v2/top-headlines?country='
+    const url = 'https://corsproxy.io/?https://newsapi.org/v2/top-headlines?country='
               + params.country +'&category='+ params.category +'&apiKey=15da07e608df4db2ab2a0600406463d1';
 
     return this.http.get(url);
